eb57baa600363357fa7734e1c4941dfd5df0cdb0
sergiobraiscompo <comesanapoloniosergiobrais@gmail.com>
2024-12-11 12:40:46 +0100

    Primera Push

--- consultas.script.sql
+++ consultas.script.sql
@@ -1,20 +1,31 @@
 -- Listar las pistas (tabla Track) con precio mayor o igual a 1€
-
+SELECT T.Name 
+FROM Track T
+Where T.UnitPrice >= 1
 
 -- Listar las pistas de más de 4 minutos de duración
-
+SELECT T.Name 
+FROM Track T 
+Where T.Milliseconds > 24000
 
 -- Listar las pistas que tengan entre 2 y 3 minutos de duración
-
+SELECT T.Name 
+FROM Track T 
+Where T.Milliseconds Between 120000 AND 180000
 
 -- Listar las pistas que uno de sus compositores (columna Composer) sea Mercury
-
+SELECT T.Name, T.Composer 
+FROM Track T 
+Where T.Composer LIKE '%Mercury%'
 
 -- Calcular la media de duración de las pistas (Track) de la plataforma
-
+SELECT AVG(T.Milliseconds) AS 'Average Running Time' 
+FROM Track T 
 
 -- Listar los clientes (tabla Customer) de USA, Canada y Brazil
-
+SELECT C.FirstName, C.Country 
+FROM Customer C 
+Where C.Country in ('USA', 'Canada', 'Brazil')
 
 -- Listar todas las pistas del artista 'Queen' (Artist.Name = 'Queen')
 
