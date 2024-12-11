ddc798ed0ad5766f697a9e51aea2a82a87b3d6ad
sergiobraiscompo <comesanapoloniosergiobrais@gmail.com>
2024-12-11 11:25:40 +0100

    Primera Push

---
+++ consultas.script.sql
@@ -0,0 +1,46 @@
+-- Listar las pistas (tabla Track) con precio mayor o igual a 1€
+
+
+-- Listar las pistas de más de 4 minutos de duración
+
+
+-- Listar las pistas que tengan entre 2 y 3 minutos de duración
+
+
+-- Listar las pistas que uno de sus compositores (columna Composer) sea Mercury
+
+
+-- Calcular la media de duración de las pistas (Track) de la plataforma
+
+
+-- Listar los clientes (tabla Customer) de USA, Canada y Brazil
+
+
+-- Listar todas las pistas del artista 'Queen' (Artist.Name = 'Queen')
+
+
+-- Listar las pistas del artista 'Queen' en las que haya participado como compositor David Bowie
+
+
+-- Listar las pistas de la playlist 'Heavy Metal Classic'
+
+
+-- Listar las playlist junto con el número de pistas que contienen
+
+
+-- Listar las playlist (sin repetir ninguna) que tienen alguna canción de AC/DC
+
+
+-- Listar las playlist que tienen alguna canción del artista Queen, junto con la cantidad que tienen
+
+
+-- Listar las pistas que no están en ninguna playlist
+
+
+-- Listar los artistas que no tienen album
+
+
+-- Listar los artistas con el número de albums que tienen
+
+
+-- Para saber si está bien, asegurate que algunos de los artistas de la query anterior (artistas sin album) aparecen en este listado con 0 albums
